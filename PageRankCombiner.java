827136d9aebda6234b88ee813409a6c3b46d7be6
summer <summer@ubuntu.indiana.edu>
2014-01-01 02:44:32 -0500

    new version of PageRank HW completed

--- PageRankCombiner.java
+++ PageRankCombiner.java
@@ -87,40 +87,61 @@ public class PageRankCombiner implements Combiner {
 	public void combine(Map<Key, Value> keyValues) throws TwisterException {
 		try {
 			int numData, numUrls;
-			DoubleVectorData tmpDvd = new DoubleVectorData();
-			Set<Integer> urlsSet = new HashSet<Integer>();
-			Key key = keyValues.keySet().iterator().next();
+            DoubleVectorData tmpDvd = new DoubleVectorData();
+            Set<Integer> urlsSet = new HashSet<Integer>();
+            Key key = keyValues.keySet().iterator().next();
 
-			BytesValue val = (BytesValue) keyValues.get(key);
-			tmpDvd.fromBytes(val.getBytes());
-			double[][] tmpData;
-			tmpData = tmpDvd.getData();
-			numUrls = (int) tmpData[0][0];
-			double[][] newPageRank = new double[numUrls][2];
-			double tanglingProb = 0.0d;
+            BytesValue val = (BytesValue) keyValues.get(key);
+            tmpDvd.fromBytes(val.getBytes());
+            numData = tmpDvd.getNumData();
+            double[][] tmpData = new double[numData][2];
+            tmpData = tmpDvd.getData();
+            numUrls = (int) tmpData[0][0];
+            double[][] newPageRank = new double[numUrls][2];
+            double tanglingProb = 0.0d;
 
-			int index; // index of changed url
-			
-			/* Write your code and COMPLETE HERE */
+            int index; // index of changed url
+            double probSum = 0.0d;
+            int step = 0;
+            for (Iterator<Key> ite = keyValues.keySet().iterator(); ite
+                            .hasNext();) {
+                    step++;
+                    key = ite.next();
+                    val = (BytesValue) keyValues.get(key);
+                    tmpDvd = new DoubleVectorData();
+                    tmpDvd.fromBytes(val.getBytes());
+                    numData = tmpDvd.getNumData();
+                    tmpData = tmpDvd.getData();
+                    tanglingProb += tmpData[0][1];
+                    probSum +=tanglingProb;
+                    for (int j = 1; j < numData; j++) {
+                            index = (int) tmpData[j][0];
+                            newPageRank[index][1] += tmpData[j][1];
+                            probSum += tmpData[j][1];        
+                            urlsSet.add(index);
+                    }// end for
+            }// for iterator
 
-			int numChangedUrls = urlsSet.size();
-			int[] urlsArray = new int[numChangedUrls];
-			Iterator<Integer> iter = urlsSet.iterator();
-			for (int i = 0; i < numChangedUrls; i++) {
-				if (iter.hasNext())
-					urlsArray[i] = (iter.next()).intValue();
-			}
+            int numChangedUrls = urlsSet.size();
+            int[] urlsArray = new int[numChangedUrls];
+            Iterator<Integer> iter = urlsSet.iterator();
+            for (int i = 0; i < numChangedUrls; i++) {
+                    if (iter.hasNext())
+                            urlsArray[i] = (iter.next()).intValue();
+            }
 
-			double[][] resultData = new double[numChangedUrls + 1][2];
-			resultData[0][0] = numUrls;
-			resultData[0][1] = tanglingProb;
-			for (int i = 1; i <= numChangedUrls; i++) {
-				resultData[i][0] = urlsArray[i - 1];
-				resultData[i][1] = newPageRank[urlsArray[i - 1]][1];
-			}
-			DoubleVectorData newPageRankDvd = new DoubleVectorData(resultData,
-					numChangedUrls + 1, 2);
-			this.results.fromBytes(newPageRankDvd.getBytes());
+            double[][] resultData = new double[numChangedUrls + 1][2];
+            resultData[0][0] = numUrls;
+            resultData[0][1] = tanglingProb;
+            double changedUrlsValues = 0.0d;
+            for (int i = 1; i <= numChangedUrls; i++) {
+                    resultData[i][0] = urlsArray[i - 1];
+                    resultData[i][1] = newPageRank[urlsArray[i - 1]][1];
+                    changedUrlsValues += resultData[i][1];
+            }
+            DoubleVectorData newPageRankDvd = new DoubleVectorData(resultData,
+                            numChangedUrls + 1, 2);
+            this.results.fromBytes(newPageRankDvd.getBytes());
 		} catch (Exception e) {
 			throw new TwisterException(e);
 		}
